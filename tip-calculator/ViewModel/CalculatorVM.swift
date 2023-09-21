2f3a1df33a8a7e02a99a17d685e58c1999ddb73d
Juan Carlos Hernandez <jchernandez0312@gmail.com>
2023-09-20 22:11:29 -0500

    Implemented logic to calculate tip on view model

--- tip-calculator/ViewModel/CalculatorVM.swift
+++ tip-calculator/ViewModel/CalculatorVM.swift
@@ -25,15 +25,39 @@ class CalculatorVM {
     
     func transform(input: Input) -> Output {
 
-        input.splitPublisher.sink { tip in
-            print(tip)
-        }.store(in: &cancellables)
+        let updateViewPublisher = Publishers.CombineLatest3(
+            input.billPublisher,
+            input.tipPublisher,
+            input.splitPublisher).flatMap {[unowned self] (bill, tip, split) in
+                let totalTip = getTipAmmount(bill: bill, tip: tip)
+                let totalBill = bill + totalTip
+                let amountPerPerson = totalBill / Double(split)
+                
+                let result = Result(
+                    amountPerPerson: amountPerPerson,
+                    totalBill: totalBill,
+                    totalTip: totalTip)
+                
+                return Just(result)
+            }.eraseToAnyPublisher()
         
-        let result = Result(
-            amountPerPerson: 500,
-            totalBill: 1000,
-            totalTip: 1500)
-        return Output(updateViewPublisher: Just(result).eraseToAnyPublisher())
+
+        return Output(updateViewPublisher: updateViewPublisher)
+    }
+    
+    private func getTipAmmount(bill: Double, tip: Tip) -> Double {
+        switch tip {
+        case .none:
+            return 0
+        case .tenPercent:
+            return bill * 0.1
+        case .fifteenPercent:
+            return bill * 0.15
+        case .twentyPercent:
+            return bill * 0.2
+        case .custom(let value):
+            return Double(value)
+        }
     }
     
 }
